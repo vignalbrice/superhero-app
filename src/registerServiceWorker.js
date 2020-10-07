6abebe89924454959c29e0d96560bc37e0b11f29
Vignal Brice <vignal.brice@gmail.com>
2020-10-08 01:45:14 +0200

    deploy

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,3 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
-
 import { register } from "register-service-worker";
 
 if (process.env.NODE_ENV === "production") {
