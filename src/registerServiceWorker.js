03a4e5060a8c6041dd64e9b66a72e022ce2513f4
Vignal Brice <vignal.brice@gmail.com>
2020-10-08 01:33:01 +0200

    fix builds errors

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,6 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
 import { register } from "register-service-worker";
 
 if (process.env.NODE_ENV === "production") {
